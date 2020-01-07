2a25fce7adf2fa2e58b3dc61485d836ccabc8828
koDaegon <daegon7897@gmail.com>
2020-01-07 23:18:23 +0900

    Update Toolbar, Nav items, SideDrawer and toggle function with responsive design

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React , {Component} from 'react';
 import Aux from '../../hoc/Auxiliary'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = props => {
-    return (
-    <Aux>
-        <Toolbar />
-        <main className = {classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-    );
-};
+class Layout extends Component {
+    
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    };
+
+    sideDrawerToggleClicked =() => {
+        this.setState((prevState) => {
+           return {showSideDrawer: !prevState.showSideDrawer};
+        })
+    };
+
+    render () {
+        return (
+        <Aux>
+            <Toolbar drawerToggleClicked= {this.sideDrawerToggleClicked}/>
+            <SideDrawer 
+            open= {this.state.showSideDrawer} 
+            clicked= {this.sideDrawerClosedHandler}/>
+            <main className = {classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+        )
+    }
+}
 
 export default Layout;
