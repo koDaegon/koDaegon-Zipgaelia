ed1df0de14ca49ae0ba563d5d707bd389bf2a43a
koDaegon <daegon7897@gmail.com>
2020-02-22 00:20:10 +0900

    Connect redux to App.js for auto login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Route ,Switch} from 'react-router-dom';
+import React, { Component } from 'react';
+import {Route ,Switch, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import './App.css';
@@ -8,22 +9,35 @@ import Checkout from './container/Checkout/Checkout';
 import Orders from './container/Orders/Orders';
 import Auth from './container/Auth/Auth';
 import Logout from './container/Auth/Logout/Logout';
+import * as action from './store/actions/index';
 
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Switch>
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/checkout" component={Checkout} /> 
-          <Route path="/orders" component={Orders} />
-          <Route path="/" component={BurgerBuilder} />
-        </Switch>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.onAutoLogin();
+  }
+  
+  render () {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/checkout" component={Checkout} /> 
+            <Route path="/orders" component={Orders} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAutoLogin: ()=>dispatch(action.authCheckState())
+  }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
